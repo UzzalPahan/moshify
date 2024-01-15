4cfa717cda34af4f5cf3a42cc65c4c1028c9c7c2
Uzzal Pahan <uzzalpahan1@gmail.com>
2024-01-15 23:19:14 +0600

    added slider on testimonial

--- app.js
+++ app.js
@@ -167,3 +167,24 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 //fetch service end
+
+
+//initialization swiper
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+//swiper end
